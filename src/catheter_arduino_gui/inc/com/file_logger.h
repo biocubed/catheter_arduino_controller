fbef3bd753b1a436bb8ff46e804fedc26c56716d
biocubed <rcj33@case.edu>
2016-10-18 16:55:01 -0400

    Adding a file logging header

---
+++ src/catheter_arduino_gui/inc/com/file_logger.h
@@ -0,0 +1,25 @@
+
+
+
+#include <vector>
+#include <cstring>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+/*
+ * This file defines functions and methods which can be used for logging file input/output
+ * The purpose is meant to make log files of the catheter currents (for later analysis)
+ */
+
+
+namespace file_logger
+{
+
+
+};
+
+
